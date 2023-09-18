44fa379def16276f59c75994a74e4b685416b0f9
Lana2408 <119637611+Lana2408@users.noreply.github.com>
2023-09-18 23:50:54 +0100

    task3

---
+++ src/components/Friends/Friends.jsx
@@ -0,0 +1,20 @@
+import css from './Friends.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => (
+        <li className={css.item} key={id}>
+           <span className={`${css.status} ${isOnline ? css.online : css.offline}`}></span>
+          <img
+            className={css.avatar}
+            src={avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p className={css.name}>{name}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
